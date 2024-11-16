a613a0c7865396e53f3625ff0d240d9c53a02dfd
danishmaknojia <danishmaknojia>
2024-11-16 03:23:14 +0000

    test

--- test_main.py
+++ test_main.py
@@ -1,41 +1,14 @@
-import requests
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
 
-# Fetch environment variables
+# Fetch a single environment variable
 server_h = os.getenv("SERVER_HOSTNAME")
-access_token = os.getenv("ACCESS_TOKEN")
-FILESTORE_PATH = "dbfs:/FileStore/danish_mini_project11"
-# change
-# Check if environment variables are loaded properly
-if not server_h or not access_token:
-    raise ValueError("Not set in the environment variables.")
 
-# Construct the URL
-url = f"https://{server_h}/api/2.0"
+# Check if it's loaded properly
+if not server_h:
+    raise ValueError("SERVER_HOSTNAME is not set in the environment variables.")
 
-# Function to check if a file path exists and auth settings still work
-def check_filestore_path(path, headers): 
-    try:
-        response = requests.get(url + f"/dbfs/get-status?path={path}", headers=headers)
-        response.raise_for_status()
-        
-        # Check if 'path' key exists in response
-        return response.json().get('path') is not None
-    except requests.exceptions.RequestException as e:
-        print(f"Error checking file path: {e}")
-        return False
-
-# Test if the specified FILESTORE_PATH exists
-def test_databricks():
-    headers = {'Authorization': f'Bearer {access_token}'}
-    if check_filestore_path(FILESTORE_PATH, headers):
-        print(f"File path {FILESTORE_PATH} exists.")
-    else:
-        print(f"File path {FILESTORE_PATH} does not exist")
-
-if __name__ == "__main__":
-    test_databricks()
+print(f"SERVER_HOSTNAME: {server_h}")
